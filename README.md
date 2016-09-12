412645d20a2a849e9a8d9520c9b73faa6567a668
Ange Picard <CodingJuice@gmail.com>
2016-09-12 09:28:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#XMLLiteParser
+XMLLiteParser is a small XML parser using simplified XML Rules.
+
---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MrMan on 12/09/2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Wow");
+    }
+}
+
