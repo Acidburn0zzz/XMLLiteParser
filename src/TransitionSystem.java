b23be9471d10379c1f20f23876430dc1ddaa5cfc
Nexyll <antoine7104@gmail.com>
2016-09-12 20:50:27 +0200

    Add Not implemented Exeption

--- src/TransitionSystem.java
+++ src/TransitionSystem.java
@@ -1,14 +1,16 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by MrMan on 12/09/2016.
  */
 public class TransitionSystem {
     public State getEvent(){
-        
+        throw new NotImplementedException();
     }
     public void next(){
 
     }
     public boolean validate(){
-
+        throw new NotImplementedException();
     }
 }
