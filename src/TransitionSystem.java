0047698670715016d9c2ea4e54863c1cfa692bea
Nexyll <antoine7104@gmail.com>
2016-09-13 23:22:16 +0200

    Add Not implemented Function

--- src/TransitionSystem.java
+++ src/TransitionSystem.java
@@ -1,51 +1,79 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.io.*;
 import java.nio.charset.Charset;
 
-/**
- * Created by MrMan on 12/09/2016.
- */
+
 public class TransitionSystem {
 
     private String filePath;
     private File file;
+    private InputStream inputStream;
     private Reader reader;
     private Reader buffer;
+    private Charset encoding;
 
+    /**
+     * @param filePath
+     */
     public TransitionSystem(String filePath) {
         this.filePath = filePath;
-        Charset encoding = Charset.defaultCharset();
+        encoding = Charset.defaultCharset();
         file = new File(filePath);
-
     }
 
-    private void handleFile(File file, Charset encoding)
-            throws IOException {
-        try (InputStream in = new FileInputStream(file);
-             Reader reader = new InputStreamReader(in, encoding);
-             // buffer for efficiency
-             buffer = new BufferedReader(reader)) {
-        }
+    /**
+     * @throws IOException
+     */
+    private void handleFile() throws IOException {
+        inputStream = new FileInputStream(file);
+        reader = new InputStreamReader(inputStream, encoding);
+        buffer = new BufferedReader(reader);
     }
 
-    private char nextChar() throws IOException {
-        return (char)buffer.read();
+    /**
+     * @return
+     * @throws Exception
+     */
+    private char nextChar() throws Exception {
+        if (buffer != null)
+            return (char)buffer.read();
+        else
+            throw new Exception("Pas de buffer à lire");
     }
+
+
     public State getEvent(){
         throw new NotImplementedException();
     }
+
+    /**
+     *
+     */
     public void next(){
         throw new NotImplementedException();
     }
+
+    /**
+     * @return
+     */
     public boolean validate(){
         throw new NotImplementedException();
     }
 
+    /*
+     * GETTERS AND SETTERS
+     */
+
+    /**
+     * @return
+     */
     public String getFilePath() {
         return filePath;
     }
 
+    /**
+     * @param filePath
+     */
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
