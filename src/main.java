1dd4bf470daa4d7228b4b4b94677e60c7a7d6773
MonsieurMan <CodingJuice@gmail.com>
2016-09-17 12:22:26 +0200

    Add elapsed time to the main

--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
 
 /**
  * Created by MrMan on 12/09/2016.
@@ -7,12 +9,14 @@ public class main {
     public static void main(String[] args) {
         TransitionSystem ts = new TransitionSystem();
         try {
-            ts.openXMLFile("");
+            ts.openXMLFile("C:/Users/MrMan/Desktop/lol.xmll");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        long startTime = System.currentTimeMillis();
+        long elapsedTime= 0L;
         ts.start();
-        System.out.println("Wow");
+        elapsedTime = (new Date()).getTime() - startTime;
+        System.out.println("Document validated in " + (elapsedTime) + " ms");
     }
 }
