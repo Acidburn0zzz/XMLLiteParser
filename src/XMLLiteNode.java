d0476ee2a629b3dd636d676265edfda55799bef4
MonsieurMan <CodingJuice@gmail.com>
2016-09-17 12:21:00 +0200

    Complete XMLLiteNode and Parser

--- src/XMLLiteNode.java
+++ src/XMLLiteNode.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+
 /**
  * Created by MrMan on 12/09/2016.
  */
 public class XMLLiteNode {
+    private String name;
+    private String content;
+    private XMLLiteNode parent;
+    private ArrayList<XMLLiteNode> children;
+    
+    public XMLLiteNode(String name) {
+        this.name = name;
+        this.parent = null;
+    }
+
+    public XMLLiteNode(String name, XMLLiteNode parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public void fillContent(String content){
+        this.content = content;
+    }
+
+    public void addChildren(XMLLiteNode node){
+        if(children == null)
+            children = new ArrayList<>();
+        children.add(node);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public XMLLiteNode getParent() {
+        return parent;
+    }
 }
--- src/XMLLiteParser.java
+++ src/XMLLiteParser.java
@@ -2,4 +2,54 @@
  * Created by MrMan on 12/09/2016.
  */
 public class XMLLiteParser {
+    private String buffer;
+    private XMLLiteNode lastNode;
+    private boolean nodeBeforeContent; //Because there can be content only before new nodes
+    private XMLLiteNode rootNode;
+
+    public XMLLiteParser() {
+        buffer = null;
+        lastNode = null;
+        nodeBeforeContent = false;
+    }
+
+    private void fillBuffer(char c){
+        buffer += c;
+    }
+
+    private void createNode(){
+        assert (!buffer.isEmpty());
+        XMLLiteNode node;
+
+        if(lastNode != null) {
+            node = new XMLLiteNode(buffer, lastNode);
+            rootNode = node;
+            lastNode.addChildren(node);
+        }else{
+            node = new XMLLiteNode(buffer);
+        }
+
+        lastNode = node;
+        buffer = null;
+        nodeBeforeContent = false;
+    }
+
+    private void closeNode(){
+        assert(buffer == lastNode.getName());
+
+        lastNode = lastNode.getParent();
+        buffer = null;
+        nodeBeforeContent = true;
+    }
+
+    private void fillNodeContent(){
+        assert(!(buffer.isEmpty() && nodeBeforeContent));
+
+        lastNode.fillContent(buffer);
+        buffer = null;
+    }
+
+    public XMLLiteNode getRootNode() {
+        return rootNode;
+    }
 }
